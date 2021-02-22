5c47faa40d642467719dddb29fa294a392e5cb5d
kfieldi5 <kfielding@lumoslabs.com>
2021-02-22 14:31:39 -0800

    Initial Commit

---
+++ speedconnections/speedconnectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  speedconnectionsApp.swift
+//  speedconnections
+//
+//  Created by kfielding on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct speedconnectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
